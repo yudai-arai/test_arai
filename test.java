4443be12c39952d0216598d089c2f719c048eed9
新井 雄大 <yarai@BPDOMAIN.local>
2019-03-14 15:35:56 +0900

    pullreq test

--- test.java
+++ test.java
@@ -1,5 +1,25 @@
 public class test{
 	public static void main(String[] args){
-	 System.out.println("Hello World");
-	}
+		int n = 0;
+		String num[] = new String[]{"10"};
+
+	 if(num.length != 1){
+	  System.out.println("ˆø”1‚Â‚Ì‚İw’è‚µ‚Ä‚­‚¾‚³‚¢");
+      System.exit(1);
+  }
+
+    try{
+      n = Integer.parseInt(num[0]);
+    }catch(NumberFormatException e){
+      System.out.println("”’l‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B");
+      System.exit(1);  
+    }
+
+  	for(int i = 0 ; i< n; i++){
+	   if(i == 1001){
+	     break;
+	   }
+	     System.out.println("Hello World");
+}
+}
 }

09a8eb5fea18199582ab492633ad9cedbca465f5
新井 雄大 <yarai@BPDOMAIN.local>
2019-03-26 22:13:45 +0900

    'インデント調整'

--- test.java
+++ test.java
@@ -3,25 +3,25 @@ public class test{
         int n = 0;
         String num[] = new String[]{};
 
-    if(num.length != 1){
-      System.out.println("引数1つのみ指定してください");
-        System.exit(1);
-}
-    try{
-      n = Integer.parseInt(num[0]);
-    }catch(NumberFormatException e){
-      System.out.println("数値を入力してください。");
-       System.exit(1);
-}
-    if(n < 0){
-      System.out.println("負の数は指定できません。");
-        System.exit(1);
-}
-    for(int i = 0 ; i< n; i++){
-      if(i == 1001){
-          break;
-}
-      System.out.println("Hello World");
-}
-}
+        if(num.length != 1){
+              System.out.println("引数1つのみ指定してください");
+            System.exit(1);
+        }
+        try{
+              n = Integer.parseInt(num[0]);
+        }catch(NumberFormatException e){
+              System.out.println("数値を入力してください。");
+               System.exit(1);
+        }
+        if(n < 0){
+              System.out.println("負の数は指定できません。");
+            System.exit(1);
+        }
+        for(int i = 0 ; i< n; i++){
+              if(i == 1001){
+                  break;
+              }
+              System.out.println("Hello World");
+        }
+    }
 }
